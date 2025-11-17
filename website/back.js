18839f0caee20f351a961e514bb582afb0993770
ItssKatua <pistikminarik@gmail.com>
2025-11-18 00:19:13 +0100

    L

---
+++ website/back.js
@@ -0,0 +1,19 @@
+const frames = ["../img/open.png", "../img/closed.png", "../img/open.png", "../img/closed.png"];
+const delays = [200, 1000];
+
+let current = 0;
+const img = document.getElementById('animated');
+
+frames.forEach(src => {
+    const i = new Image();
+    i.src = src;
+});
+
+function nextFrame() {
+    img.src = frames[current];
+
+    const delay = (current === 3) ? 1000 : 200;
+    current = (current + 1) % frames.length;
+    setTimeout(nextFrame, delay);
+}
+nextFrame();
